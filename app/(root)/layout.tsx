e26dc503df25037b88b729b391719d622ca87826
inba7399 <inbasagar7@gmail.com>
2024-11-20 05:58:38 +0530

    first commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navebar from '../components/Navebar'
+
+function layout({children}: Readonly<{children: React.ReactNode}>) {
+  return (
+  <main className='font-work-sans'>
+   <Navebar/>
+  {children}
+  </main>
+  )
+}
+
+export default layout
---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+ console.log("werfjaeuirhf")
+  return (
+    
+   <>
+    <h1 className="text-3">Home</h1>
+   </>
+  );
+}
